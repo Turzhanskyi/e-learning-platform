42e100e7f411460a7b809f786fb79affc7e98c6e
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2020-12-18 16:37:01 +0200

    Add rich text editor (action_text)

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,8 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5 }
 
+  has_rich_text :description
+
   def to_s
     title
   end
