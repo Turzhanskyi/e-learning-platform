98543700a8bad7a9c642a5e19facf6e8157c9b83
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2020-12-19 17:37:03 +0200

    Add sendgrid - send emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "e_learning_platform_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'e-learning-platform-turvitan.herokuapp.com',
+                                               protocol: 'https' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
