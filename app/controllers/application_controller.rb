7d70b213a50b7d0e257dc95ec2b103b3deba13d3
TurVitAn <turzhansky81@gmail.com>
2020-12-22 18:21:14 +0200

    Add check if current_user is online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,23 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_global_variables, if: :user_signed_in?
+  after_action :user_activity
 
   include Pundit
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   include PublicActivity::StoreController # save current_user using gem public_activity
 
-  before_action :set_global_variables, if: :user_signed_in?
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search) # navbar search
   end
 
   private
+
+  def user_activity
+    current_user.try :touch
+  end
 
   # pundit
   def user_not_authorized
