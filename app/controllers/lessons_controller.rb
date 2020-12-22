6f82cb7e54d90e2a82759f32660068796f9ea261
TurVitAn <turzhansky81@gmail.com>
2020-12-23 00:58:49 +0200

    Add authorization for lessons (gem pundit)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,13 +7,17 @@ class LessonsController < ApplicationController
     @lessons = Lesson.all
   end
 
-  def show; end
+  def show
+    authorize @lesson
+  end
 
   def new
     @lesson = Lesson.new
   end
 
-  def edit; end
+  def edit
+    authorize @lesson
+  end
 
   def create
     @lesson = Lesson.new(lesson_params)
@@ -30,6 +34,7 @@ class LessonsController < ApplicationController
   end
 
   def update
+    authorize @lesson
     respond_to do |format|
       if @lesson.update(lesson_params)
         format.html { redirect_to @lesson, notice: 'Lesson was successfully updated.' }
@@ -42,6 +47,7 @@ class LessonsController < ApplicationController
   end
 
   def destroy
+    authorize @lesson
     @lesson.destroy
     respond_to do |format|
       format.html { redirect_to lessons_url, notice: 'Lesson was successfully destroyed.' }
