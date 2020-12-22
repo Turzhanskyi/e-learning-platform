a145ecda96e06df4ff7321440e553c17cbe43e5e
TurVitAn <turzhansky81@gmail.com>
2020-12-22 16:23:58 +0200

    Add authorization for courses (gem pundit)

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,6 +12,7 @@ class CoursesController < ApplicationController
 
   def new
     @course = Course.new
+    authorize @course
   end
 
   def edit
@@ -20,6 +21,7 @@ class CoursesController < ApplicationController
 
   def create
     @course = Course.new(course_params)
+    authorize @course
     @course.user = current_user
 
     respond_to do |format|
@@ -34,6 +36,7 @@ class CoursesController < ApplicationController
   end
 
   def update
+    authorize @course
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
@@ -46,6 +49,7 @@ class CoursesController < ApplicationController
   end
 
   def destroy
+    authorize @course
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
