b7c36d62291f84c4a3f15fb81e4974205db9b491
TurVitAn <turzhansky81@gmail.com>
2020-12-22 12:54:30 +0200

    Add roles to users (gem rolify)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,26 @@ class User < ApplicationRecord
 
   has_many :courses
 
+  rolify
+
   def to_s
     email
   end
 
   def username
     email.split(/@/).first
+  end
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      add_role(:admin) if roles.blank?
+      add_role(:teacher)
+      add_role(:student)
+    else
+      add_role(:student) if roles.blank?
+      add_role(:teacher) # if you want any user to be able to create own courses
+    end
   end
 end
