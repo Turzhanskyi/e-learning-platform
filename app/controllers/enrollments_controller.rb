0889b9e29751bce0f6ba465cff4be0ec7bf8e5b0
TurVitAn <turzhansky81@gmail.com>
2020-12-24 23:53:44 +0200

    Add pagination for enrollments and users (gem pagy)

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,7 +5,7 @@ class EnrollmentsController < ApplicationController
   before_action :set_course, only: %i[new create]
 
   def index
-    @enrollments = Enrollment.all
+    @pagy, @enrollments = pagy(Enrollment.all)
     authorize @enrollments
   end
 
