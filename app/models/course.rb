31225acfd0aa2fe11faa712a5f592f82a948bfae
TurVitAn <turzhansky81@gmail.com>
2020-12-25 19:00:19 +0200

    Add calculate rating for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -24,6 +24,14 @@ class Course < ApplicationRecord
     enrollments.where(user_id: [user.id], course_id: [id]).empty?
   end
 
+  def update_rating
+    if enrollments.any? && enrollments.where.not(rating: nil).any?
+      update_column :average_rating, enrollments.average(:rating).round(2).to_f
+    else
+      update_column :average_rating, 0
+    end
+  end
+
   LANGUAGES = %i[English Ukrainian Russian Polish].freeze
   def self.languages
     LANGUAGES.map { |language| [language, language] }
