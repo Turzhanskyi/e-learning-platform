640eadd4c6adebee8d1916e1647ac78d289d9d2b
TurVitAn <turzhansky81@gmail.com>
2020-12-25 17:24:11 +0200

    Add scopes for Courses and Enrollments (gem ransack, pagy)

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get 'activity', to: 'home#activity'
+  resources :users, only: %i[index edit show update]
   resources :courses do
+    get :purchased, :pending_review, :created, on: :collection
     resources :lessons
     resources :enrollments, only: %i[new create]
   end
-  resources :users, only: %i[index edit show update]
-  resources :enrollments
+  resources :enrollments do
+    get :my_students, on: :collection
+  end
 end
