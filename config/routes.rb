3d2908bfd30b49064dd915d6027034c148a7020b
TurVitAn <turzhansky81@gmail.com>
2020-12-26 18:04:40 +0200

    Add load charts without timeouts

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
+  get 'charts/users_per_day', to: 'charts#users_per_day'
+  get 'charts/enrollments_per_day', to: 'charts#enrollments_per_day'
+  get 'charts/course_popularity', to: 'charts#course_popularity'
   resources :users, only: %i[index edit show update]
   resources :courses do
     get :purchased, :pending_review, :created, on: :collection
