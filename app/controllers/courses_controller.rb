01d1b9f5abf292df1fa5f62cf1a2bd2ca0f5f23c
TurVitAn <turzhansky81@gmail.com>
2020-12-27 05:44:35 +0200

    Add complex validation who can see a course show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -61,6 +61,8 @@ class CoursesController < ApplicationController
   end
 
   def show
+    authorize @course
+
     @lessons = @course.lessons
     @enrollments_with_review = @course.enrollments.reviewed
   end
