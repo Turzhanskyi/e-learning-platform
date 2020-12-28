e717650e1b91d121c16d9f7ede8e80d333865bec
TurVitAn <turzhansky81@gmail.com>
2020-12-28 15:46:44 +0200

    Add minor styling

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
-  validates :title, :short_description, :language, :price, :level, presence: true
-  validates :title, uniqueness: true
-  validates :description, presence: true, length: { minimum: 5 }
+  validates :title, :description, :short_description, :language, :price, :level, presence: true
+  validates :description, length: { minimum: 5 }
+  validates :short_description, length: { maximum: 300 }
 
   belongs_to :user, counter_cache: true
   # User.find_each { |user| User.reset_counters(user.id, :courses) }
