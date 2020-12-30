0cea480f6691d417bacf3ba2d87cb50a4d668efa
TurVitAn <turzhansky81@gmail.com>
2020-12-31 01:22:44 +0200

    Add play audio with HTML5 (action_text)

--- app/javascript/packs/trix-editor-overrides.js
+++ app/javascript/packs/trix-editor-overrides.js
@@ -1,9 +1,8 @@
 // Blacklist all file attachments
-
-// window.addEventListener("trix-file-accept", function(event) {
-//   event.preventDefault()
-//   alert("File attachment not supported!")
-// })
+//window.addEventListener("trix-file-accept", function(event) {
+//  event.preventDefault()
+//  alert("File attachment not supported!")
+//})
 
 
 // Only images
@@ -17,7 +16,6 @@
 //})
 
 // File size
-
 window.addEventListener("trix-file-accept", function(event) {
   const maxFileSize = 3000 * 3000 // around 9MB
   if (event.file.size > maxFileSize) {
