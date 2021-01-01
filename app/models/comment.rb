2e604958d42804ff3a19aead22d3c985a2c64e62
TurVitAn <turzhansky81@gmail.com>
2021-01-02 01:40:04 +0200

    Add public_activity for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,11 @@ class Comment < ApplicationRecord
   # User.find_each { |user| User.reset_counters(user.id, :comments) }
   belongs_to :lesson, counter_cache: true
   # Lesson.find_each { |lesson| Lesson.reset_counters(lesson.id, :comments) }
+
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
+
+  def to_s
+    content
+  end
 end
