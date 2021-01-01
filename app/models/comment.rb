3496ffa850e7b5395822d4cdfc166067a6369a80
TurVitAn <turzhansky81@gmail.com>
2021-01-02 00:47:48 +0200

    Add counter_cache and delete comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@
 class Comment < ApplicationRecord
   validates :content, presence: true
 
-  belongs_to :user
-  belongs_to :lesson
+  belongs_to :user, counter_cache: true
+  # User.find_each { |user| User.reset_counters(user.id, :comments) }
+  belongs_to :lesson, counter_cache: true
+  # Lesson.find_each { |lesson| Lesson.reset_counters(lesson.id, :comments) }
 end
