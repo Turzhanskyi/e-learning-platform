d016745745d8c85af307409476f3d0c3341993d1
TurVitAn <turzhansky81@gmail.com>
2021-01-02 11:42:45 +0200

    Add recaptcha with devise

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Recaptcha.configure do |config|
+  config.site_key = '6LeDvh0aAAAAAGndfrvka7ctsJSqkXUoX_4HL5Fv'
+  config.secret_key = '6LeDvh0aAAAAAForvc6Ehlt8X6GxvIxbhPMmzVqE'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
