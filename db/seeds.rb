53239b22371a9c9b8ef1474982dd8379089f1b72
TurVitAn <turzhansky81@gmail.com>
2021-01-07 20:41:46 +0200

    Add helper logic to view certificate of completion

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,6 @@ if User.find_by_email('admin@example.com').nil?
   admin.add_role(:admin) unless admin.has_role?(:admin)
 end
 
-if User.find_by_email('teacher@example.com').nil?
-  teacher = User.create!(email: 'teacher@example.com', password: '123456',
-                         password_confirmation: '123456', confirmed_at: Time.now)
-  teacher.add_role(:teacher) unless teacher.has_role?(:teacher)
-end
-
 if User.find_by_email('student@example.com').nil?
   student = User.create!(email: 'student@example.com', password: '123456', password_confirmation: '123456',
                          confirmed_at: Time.now)
@@ -29,21 +23,7 @@ PublicActivity.enabled = false
                    user: User.find_by(email: 'admin@example.com'),
                    language: Faker::ProgrammingLanguage.name,
                    level: 'Beginner',
-                   price: Faker::Number.between(from: 0, to: 20_000),
-                   approved: true,
-                   published: true
-                 }])
-end
-
-5.times do
-  Course.create!([{
-                   title: Faker::Educator.course_name,
-                   short_description: Faker::Quote.famous_last_words,
-                   description: Faker::TvShows::GameOfThrones.quote,
-                   user: User.find_by(email: 'teacher@example.com'),
-                   language: Faker::ProgrammingLanguage.name,
-                   level: 'Beginner',
-                   price: Faker::Number.between(from: 0, to: 20_000),
+                   price: 0,
                    approved: true,
                    published: true
                  }])
@@ -66,3 +46,18 @@ Course.all.each do |course|
 end
 
 PublicActivity.enabled = true
+
+# update friendly_id
+User.find_each(&:save)
+Course.find_each(&:save)
+Lesson.find_each(&:save)
+Enrollment.find_each(&:save)
+
+# update counter_cache
+Course.find_each { |course| Course.reset_counters(course.id, :enrollments) }
+User.find_each { |user| User.reset_counters(user.id, :courses) }
+User.find_each { |user| User.reset_counters(user.id, :enrollments) }
+Course.find_each { |course| Course.reset_counters(course.id, :lessons) }
+User.find_each { |user| User.reset_counters(user.id, :comments) }
+Lesson.find_each { |lesson| Lesson.reset_counters(lesson.id, :comments) }
+Tag.find_each { |tag| Tag.reset_counters(tag.id, :course_tags) }
