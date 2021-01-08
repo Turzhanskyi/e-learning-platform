ddf6bfa2251c23725ac57aeba493f328a11f5783
TurVitAn <turzhansky81@gmail.com>
2021-01-08 17:22:00 +0200

    Add edit and update functionality (gem wicked)

--- app/controllers/courses/course_wizard_controller.rb
+++ app/controllers/courses/course_wizard_controller.rb
@@ -3,17 +3,31 @@
 module Courses
   class CourseWizardController < ApplicationController
     include Wicked::Wizard
-    before_action :set_progress, only: [:show]
+    before_action :set_progress, only: %i[show update]
+    before_action :set_course, only: %i[show update finish_wizard_path]
 
     steps :basic_info, :details
 
     def show
-      @course = Course.friendly.find params[:course_id]
+      case step
+      when :details
+        @tags = Tag.all
+      end
       render_wizard
     end
 
+    def update
+      case step
+      when :basic_info
+        @course.update_attributes(course_params)
+      when :details
+        @tags = Tag.all
+        @course.update_attributes(course_params)
+      end
+      render_wizard @course
+    end
+
     def finish_wizard_path
-      @course = Course.friendly.find params[:course_id]
       course_path(@course)
     end
 
@@ -25,6 +39,15 @@ module Courses
                   else
                     0
                   end
+    end
+
+    def set_course
+      @course = Course.friendly.find params[:course_id]
+    end
+
+    def course_params
+      params.require(:course).permit(:title, :description, :short_description, :price,
+                                     :published, :language, :level, :avatar, tag_ids: [])
     end
   end
 end
